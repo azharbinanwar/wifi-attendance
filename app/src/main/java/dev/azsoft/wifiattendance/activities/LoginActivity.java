4886294c38592d1096da69af29c6794de5affa58
Azhar Ali <azharbinanwar>
2022-05-31 01:38:04 +0500

    UI
    - Complete Login
    - Complete custom view
    - Create reset password dialog

--- app/src/main/java/dev/azsoft/wifiattendance/activities/LoginActivity.java
+++ app/src/main/java/dev/azsoft/wifiattendance/activities/LoginActivity.java
@@ -1,12 +1,16 @@
 package dev.azsoft.wifiattendance.activities;
 
+import static dev.azsoft.wifiattendance.global.Const.DEFAULT_DIALOG_WIDTH;
+
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 import dev.azsoft.wifiattendance.databinding.ActivityLoginBinding;
+import dev.azsoft.wifiattendance.items.ForgotPasswordItem;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
@@ -17,7 +21,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView((binding = ActivityLoginBinding.inflate(getLayoutInflater())).getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
         init();
-        System.out.println("LoginActivity.onCreate: " + binding.llll.getLayoutParams().height  + " " +binding.llll.getLayoutParams().width);
+        binding.btnForgotPassword.setOnClickListener(view -> {
+            new ForgotPasswordItem(DEFAULT_DIALOG_WIDTH, ViewGroup.LayoutParams.WRAP_CONTENT).show(getSupportFragmentManager(), ForgotPasswordItem.TAG);
+        });
 
     }
 
